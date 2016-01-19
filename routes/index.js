a8f6fda64263c6e29eecf605529ec4777e10387b
edri <mig.santamar@gmail.com>
2016-01-19 13:26:23 +0100

    Add policy page.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var applicationName = "Swipe.Me";
 
 /* GET home page. */
-
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Swipe.Me' });
+  res.render('index', { title: applicationName });
+});
+
+/* GET policy page. */
+router.get('/policy', function(req, res) {
+  res.render('policy', { title: 'Policy', applicationName: applicationName });
 });
 
 module.exports = router;
